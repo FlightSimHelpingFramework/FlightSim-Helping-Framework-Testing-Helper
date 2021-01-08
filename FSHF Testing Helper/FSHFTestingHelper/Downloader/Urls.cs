85eba252123e9f7d11ea304526df97f0a7a4b238
AndrewFlightsim <andrewflightsim.commits@gmail.com>
2021-01-08 20:40:07 +0300

    Initial code adding

---
+++ FSHF Testing Helper/FSHFTestingHelper/Downloader/Urls.cs
@@ -0,0 +1,71 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// ------------------------------------------------------------------------------------------------------
+
+#region Usings
+
+#endregion
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace FSHFTestingHelper.Downloader
+{
+    public static class Urls
+    {
+        public static readonly string[] InvalidStringUrlsCollection =
+        {
+            "127.0.0.1",
+            "www.example.com",
+            "example.com"
+        };
+
+
+        public static readonly string[] ValidStringUrlsCollection =
+        {
+            "http://www.example.com",
+            "https://www.example.com",
+
+            "http://example1.example.com",
+            "https://example1.example.com",
+
+            "http://www.example.com/page",
+            "https://www.example.com/page",
+
+            "http://www.example.com/page?id=1&product=2",
+            "https://www.example.com/page?id=1&product=2",
+
+            "http://www.example.com/page#start",
+            "https://www.example.com/page#start",
+
+            "http://www.example.com:8080",
+            "https://www.example.com:8080",
+
+            "http://127.0.0.1",
+            "https://127.0.0.1",
+
+            "ftp://127.0.0.1"
+        };
+
+        public static readonly Uri[] ValidUrlsCollection =
+        {
+            new Uri("http://www.example.com"),
+            new Uri("https://www.example.com"),
+
+            new Uri("http://example1.example.com"),
+
+            new Uri("http://www.example.com/page"),
+
+            new Uri("http://www.example.com/page?id=1&product=2"),
+
+            new Uri("http://www.example.com/page#start"),
+
+            new Uri("http://www.example.com:8080"),
+
+            new Uri("http://127.0.0.1")
+        };
+    }
+}
---
+++ FSHF Testing Helper/FSHFTestingHelper/IcaoCodes/IcaoCodes.cs
@@ -0,0 +1,49 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// ------------------------------------------------------------------------------------------------------
+
+#region Usings
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace FSHFTestingHelper.IcaoCodes
+{
+    public static class IcaoCodes
+    {
+        public static readonly object[] CollectionsWithInvalidStringIcaoCodes =
+        {
+            new[] {new List<string> {"uUEE"}},
+            new[] {new List<string> {"", " ", "E"}},
+            new[] {new List<string> {"UUE_E", "1EDDF", "M2UHA"}},
+            new[] {new List<string> {"UUErE", "EDDafF", "EDdDT", "%LOaWI"}}
+        };
+
+        public static readonly object[] CollectionsWithMixedValidAndInvalidStringIcaoCodes =
+        {
+            new[] {new List<string> {"UUEE", null}},
+            new[] {new List<string> {"UMKK", "UUEe"}},
+            new[] {new List<string> {"UUEE", "EDDF", "EDDF_"}},
+            new[] {new List<string> {"UUE3", "EDDF12", " "}}
+        };
+
+        public static readonly object[] CollectionsWithValidStringIcaoCodes =
+        {
+            new[] {new List<string> {"UUEE"}},
+            new[] {new List<string> {"UMKK", "UEEE", "EGLL"}},
+            new[] {new List<string> {"UUEE", "EDDF", "MUHA"}},
+            new[] {new List<string> {"UUEE", "EDDF", "EDDT", "LOWI"}}
+        };
+
+        public static readonly string[] InvalidStringIcaoCodesCollection =
+        {
+            null, "", " ", "KLaX", "_2340g", "UUEЁ", "%$#*", " UUEE"
+        };
+
+        public static readonly string[] ValidStringIcaoCodesCollection =
+        {
+            "UUEE", "EDDF", "EGLL", "KLAX", "KPHX", "MUHA"
+        };
+    }
+}
